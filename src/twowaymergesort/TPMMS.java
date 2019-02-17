75cf7b9c36956026fe765ea2104d3dacdb9ebf8c
SathishChanda <nanisani007@gmail.com>
2019-02-17 16:19:55 -0500

    Write functionality added

--- src/twowaymergesort/TPMMS.java
+++ src/twowaymergesort/TPMMS.java
@@ -17,13 +17,15 @@ public class TPMMS {
 		// TODO Auto-generated method stub
 		Runtime.getRuntime().gc();
 
-		File file = new File(".." + "input.txt");
+		File file = new File("C:\\Users\\SATISH\\Desktop\\AdvanceDBProject\\AdvaceDBLabAssignment\\src\\input.txt");
 		BufferedReader br = new BufferedReader(new FileReader(file));
 		List<String> li = new ArrayList<String>();
 		Scanner scan = new Scanner(System.in);
 		System.out.print("Enter the RAM size : ");
 		int ram_size = scan.nextInt();
 		String line = br.readLine();
+		// System.out.println(line);
+		// System.out.println(line.length());
 		int record_size = line.length();
 		int count = 1;
 		int file_count = 0;
@@ -33,6 +35,7 @@ public class TPMMS {
 		// List<List<String>> data = new ArrayList<>();
 		int[] startIndices = { 0, 8, 18, 27, 52, 202, 230, 232, 241, 250 };
 		while (line != null) {
+			System.out.println(lines_read + " " + line + " " + line.length());
 			List<String> record = new ArrayList<String>();
 			for (int i = 0; i < startIndices.length - 1; i++)
 				record.add(line.substring(startIndices[i], startIndices[i + 1]).trim());
@@ -41,6 +44,7 @@ public class TPMMS {
 				Collections.sort(data, new DesendingOrderAmountPaidComparator());
 				Collections.sort(data, new AscendingOrderInsuredItem());
 				writeIntoFile(file_count++);
+				data.clear();
 			}
 			line = br.readLine();
 			lines_read++;
@@ -51,14 +55,17 @@ public class TPMMS {
 	}
 
 	public static void writeIntoFile(int file_no) throws Exception {
-		File file = new File("PART-" + file_no);
+		File file = new File("PART-" + file_no + ".txt");
 		if (!file.exists()) {
 			file.createNewFile();
 		}
 		FileWriter fileWriter = new FileWriter(file.getAbsolutePath());
 		BufferedWriter bufferedWriter = new BufferedWriter(fileWriter);
-		// for(int i=0;i<data.size();i++)
-		// bufferedWriter.write(String.join(data);
-
+		for (int i = 0; i < data.size(); i++) {
+			bufferedWriter.write(String.join("#", data.get(i)) + "\n");
+			// System.out.println(String.join("#",data.get(i)));
+		}
+		bufferedWriter.close();
+		fileWriter.close();
 	}
 }
