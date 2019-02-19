edd2e67a8a14efec242ea0a0ff1ddad5b385782a
SathishChanda <nanisani007@gmail.com>
2019-02-18 23:16:44 -0500

    Merging functions

--- src/twowaymergesort/TPMMS.java
+++ src/twowaymergesort/TPMMS.java
@@ -3,8 +3,10 @@ package twowaymergesort;
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.FileWriter;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
@@ -12,26 +14,26 @@ import java.util.Scanner;
 
 public class TPMMS {
 	public static List<List<String>> data = new ArrayList<>();
-
-	public static void main(String[] args) throws Exception {
+    public static int lines_read = 1;
+    public static int file_count = 0;
+    public static String line;
+	public static void main(String[] args) throws Exception{
 		// TODO Auto-generated method stub
 		Runtime.getRuntime().gc();
-
-		File file = new File(
-				"D:\\Concordia\\COMP 6521 ADV DATABASE TECH AND APPL\\LabAssignment1Project\\AdvaceDBLabAssignment\\src\\input.txt");
-
+		File file = new File("C:\\Users\\SATISH\\Desktop\\AdvanceDBProject\\AdvaceDBLabAssignment\\bin\\input.txt");
 		BufferedReader bufferReader = new BufferedReader(new FileReader(file));
 		Scanner scan = new Scanner(System.in);
 		System.out.print("Enter the RAM size : ");
 		int ram_size = scan.nextInt();
 		scan.close();
-		String line = bufferReader.readLine();
+		//String line = bufferReader.readLine();
+		line = bufferReader.readLine();
 		// System.out.println(line);
 		// System.out.println(line.length());
 		int record_size = line.length();
 		int count = 1;
-		int file_count = 0;
-		int lines_read = 1;
+		//int file_count = 0;
+		//int lines_read = 1;
 		int numberOfLinesToRead = ram_size * 1024 * 1024 / (16 * record_size);
 		System.out.println("Record Size = " + record_size + " Lines to Read = " + numberOfLinesToRead);
 		// List<List<String>> data = new ArrayList<>();
@@ -45,49 +47,74 @@ public class TPMMS {
 			if (lines_read % numberOfLinesToRead == 0) {
 				Collections.sort(data, new DesendingOrderAmountPaidComparator());
 				Collections.sort(data, new AscendingOrderInsuredItem());
-				writeIntoFile(file_count++);
+				writeIntoFile(file_count++,false);
 				data.clear();
 			}
 			line = bufferReader.readLine();
 			lines_read++;
 		}
 		if (data.size() > 0)
-			writeIntoFile(file_count++);
+			writeIntoFile(file_count++,false);
 		data.clear();
+		System.out.println("File Count : "+file_count);
+		System.out.println("Number of line to read : "+numberOfLinesToRead);
 		int noOfRecordsPerFile = numberOfLinesToRead / file_count;
-		int i = 0;
-		while (i < numberOfLinesToRead) {
-			for (int j = 0; j < file_count; j++) {
-				int counter = 0;
-				bufferReader = new BufferedReader(new FileReader(new File(
-						"D:\\Concordia\\COMP 6521 ADV DATABASE TECH AND APPL\\LabAssignment1Project\\AdvaceDBLabAssignment\\PART-"
-								+ j + ".txt")));
-
-				while (counter < noOfRecordsPerFile && !bufferReader.readLine().trim().equalsIgnoreCase("")) {
-
-					String[] val = bufferReader.readLine().split("#");
-					i++;
-					List<String> record = new ArrayList<String>();
-					for (String value : val)
-						record.add(value);
-					data.add(record);
-					counter++;
+		System.out.println("Number of records from a file : "+noOfRecordsPerFile);
+		mergeFiles(noOfRecordsPerFile);
+		System.out.println("Merging done!");
+	}
+	public static void mergeFiles(int noOfRecordsPerFile) throws Exception
+    {
+		int count = 0;
+		List<Integer> files_list = new ArrayList<Integer>();
+		for(int i=0;i<file_count;i++)
+			files_list.add(i);
+		BufferedReader[] bufferedReaders = new BufferedReader[file_count];
+		File merge_file = new File("PART-99.txt");
+		if(merge_file.delete())
+		{
+			System.out.println("Merge file deleted!");
+		}
+		else
+		{
+			System.out.println("file does not exist!");
+		}
+		for(int i=0;i<file_count;i++)
+			bufferedReaders[i] = new BufferedReader(new FileReader("PART-"+i+".txt"));
+		while(files_list.size()!=0) 
+		{
+			System.out.println(files_list.size());
+			for(int j = 0; j < files_list.size(); j++) {
+				line = bufferedReaders[files_list.get(j)].readLine();
+				while (count < noOfRecordsPerFile && line!=null ) {
+					   List<String> record = new ArrayList<String>();   
+					   for(String attribute : line.trim().split("#"))
+					       record.add(attribute);
+					   data.add(record);
+					   line = bufferedReaders[files_list.get(j)].readLine();
+					   if(line==null)
+					   {
+						   files_list.remove(files_list.get(j));
+						   break;
+					   }
+					   //count++;
+					   //counter++;	   
 				}
 			}
 			Collections.sort(data, new DesendingOrderAmountPaidComparator());
 			Collections.sort(data, new AscendingOrderInsuredItem());
-			writeIntoFile(99);
+			writeIntoFile(99,true);
 			data.clear();
 		}
-
-	}
-
-	public static void writeIntoFile(int file_no) throws Exception {
+        //System.out.println(lines_read);
+        //System.out.println(count);
+    }
+	public static void writeIntoFile(int file_no,boolean append) throws Exception {
 		File file = new File("PART-" + file_no + ".txt");
 		if (!file.exists()) {
 			file.createNewFile();
 		}
-		FileWriter fileWriter = new FileWriter(file.getAbsolutePath());
+		FileWriter fileWriter = new FileWriter(file.getAbsolutePath(),append);
 		BufferedWriter bufferedWriter = new BufferedWriter(fileWriter);
 		for (int i = 0; i < data.size(); i++) {
 			bufferedWriter.write(String.join("#", data.get(i)) + "\n");
